4931fca6a205b23a4a0aa28a5e47cf0cc2847693
Kevin G. Kayisire <kaykevin1@gmail.com>
2017-11-11 18:15:58 +0200

    second

--- index.php
+++ index.php
@@ -4,36 +4,39 @@ $accessToken = 'EAAHsgREoySkBAIgCvPIjZCkFB4HMpoVhH2hqLHzCF87iv3VmSK4zTabqn2gfuwR
 if(isset($_REQUEST['hub_challenge'])){
     $challenge = $_REQUEST['hub_challenge'];
     $token = $_REQUEST['hub_verify_token'];
-
-    if($token == "developercircles"){
-        echo $challenge;
-    }
 }
 
-$input = json_decode(file_get_contents('php://input'));
+if($token == "developercircles"){
+    echo $challenge;
+    die();
+}
 
-$userID = $input['entry'][0]['messaging'][0]['sender']['id'];
-$message = $input['entry'][0]['messaging'][0]['sender']['text'];
-
-echo $userID." said ".$message;
-
-$url ='https://graph.facebook.com/2.6/me/message?access_token=$accessToken';
-
-$jsonData = "{
-    'recipient': {
-        'id': $userId
-    },
-    'message': {
-        'text': 'hello there, welcome to Sight It!'
+if(file_get_contents('php://input')){
+    $input = json_decode(file_get_contents('php://input'));
+    
+    $userID = $input['entry'][0]['messaging'][0]['sender']['id'];
+    $message = $input['entry'][0]['messaging'][0]['sender']['text'];
+    
+    echo $userID." said ".$message;
+    
+    $url ='https://graph.facebook.com/2.6/me/message?access_token=$accessToken';
+    
+    $jsonData = "{
+        'recipient': {
+            'id': $userId
+        },
+        'message': {
+            'text': 'hello there, welcome to Sight It!'
+        }
+    }";
+    
+    $ch = curl_init($url);
+    
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    
+    if(!empty($input['entry'][0]['messaging'][0]['message'])){
+        curl_exec($ch);
     }
-}";
-
-$ch = curl_init($url);
-
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-
-if(!empty($input['entry'][0]['messaging'][0]['message'])){
-    curl_exec($ch);
 }
