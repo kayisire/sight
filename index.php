589abc6d8b311ddcb04190431fb55ae84ae8367f
Kevin G. Kayisire <kaykevin1@gmail.com>
2017-11-11 18:03:09 +0200

    second

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+$accessToken = '';
+
 if(isset($_REQUEST['hub_challenge'])){
     $challenge = $_REQUEST['hub_challenge'];
     $token = $_REQUEST['hub_verify_token'];
@@ -8,5 +10,30 @@ if(isset($_REQUEST['hub_challenge'])){
     }
 }
 
-$input = file_get_contents('php://input');
-var_dump($input);
+$input = json_decode(file_get_contents('php://input'));
+
+$userID = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['sender']['text'];
+
+echo $userID." said ".$message;
+
+$url ='https://graph.facebook.com/2.6/me/message?access_token=$accessToken';
+
+$jsonData = "{
+    'recipient': {
+        'id': $userId
+    },
+    'message': {
+        'text': 'hello there, welcome to Sight It!'
+    }
+}";
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+    curl_exec($ch);
+}
