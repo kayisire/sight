ecfc0a418b3fcedc7acd748ecdf682b85c13ef6d
Kevin G. Kayisire <kaykevin1@gmail.com>
2017-11-11 17:40:07 +0200

    test

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sight IT</title>
+</head>
+
+<body>
+    <h1>Welcome to Sight IT bot</h1>
+</body>
+
+</html>
 <?php
-    echo "Welcome to sight it bot!";
+if(isset($_REQUEST['hub_challenge'])){
+    $challenge = $_REQUEST['hub_challenge'];
+    $token = $_REQUEST['hub_verify_token'];
+
+    if($token == "developercircles"){
+        echo $challenge;
+    }
+}
+?>
